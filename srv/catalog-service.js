05c3c9418651205518ec13e617189764d49f730b
Gustavo A. B. <gu.mivvi@gmail.com>
2025-05-14 14:00:15 -0300

    feito o código com entities, serviço e uma action

---
+++ srv/catalog-service.js
@@ -0,0 +1,20 @@
+const cds = require('@sap/cds');
+
+module.exports = cds.service.impl(function () {
+    const { Books } = this.entities;
+
+    this.on('reabastecer', async (req) => {
+
+        const { ID, quantidade } = req.data;
+
+        if (!quantidade || quantidade <= 0) {
+            return req.error(400, 'Quantidade inválida!');
+        }
+
+        const book = await SELECT.one.from(Books).where({ID});
+        if (!book) return req.error(404, 'Livro não encontrado');
+
+        await UPDATE(Books).set({ stock: book.stock + quantidade }).where({ID});
+        return { message: `Livro reabastecido com ${quantidade} unidades` }
+    })
+})
